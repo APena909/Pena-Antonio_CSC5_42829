e2eb0268afce81ef587f67d03603242ca447c8aa
APena909 <apena76@student.rcc.edu>
2016-02-24 09:32:25 -0800

    Signed-off-by: APena909 <apena76@student.rcc.edu>

---
+++ Lab/Lab 2-17-16/HelloWorld_v2/main.cpp
@@ -0,0 +1,35 @@
+/* 
+ * File:   main.cpp
+ * Author: Antonio Pena
+ * Created on February 17, 2016, 9:45 AM
+ * Purpose: Our first program
+ */
+
+//System Libraries
+#include <iostream>
+//using namespace std;
+
+//User Libraries
+
+//Global Constants
+
+//Function Prototypes
+
+//Execution Begins Here!
+int main(int argc, char** argv) {
+    //Declare Variables
+    
+    //Initialize Variables
+    
+    //Input Values
+    
+    //Map the inputs to the outputs
+    
+    //Output the results
+    std::cout<<"Hello World"<<std::endl;
+    
+    //Exit Stage Right!
+
+    return 0;
+}
+
