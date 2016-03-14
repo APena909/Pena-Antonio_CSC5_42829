e05640f5e2b90e39c96822e8789518a9abbe3486
APena909 <apena76@student.rcc.edu>
2016-03-14 10:56:09 -0700

    Signed-off-by: APena909 <apena76@student.rcc.edu>

---
+++ Lab/Lab03-14-16/Truth Table/main.cpp
@@ -0,0 +1,27 @@
+/*
+ * Antonio Peña
+ * March 14, 2016
+ * Purpose: Output a Truth Table for Boolean Expressions.
+ */
+
+//System Libraries
+#include <iostream>
+using namespace std;
+
+//User Libraries
+
+//Global Constants
+
+//Function Prototypes
+
+//Execution Begins Here
+int main(int argc, char** argv) {
+    //Declare Variables
+    
+
+    
+    
+
+    return 0;
+}
+
