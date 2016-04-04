c2d27bd960755a262954fddb31752f63f9d22bab
APena909 <apena76@student.rcc.edu>
2016-04-04 10:03:16 -0700

    Signed-off-by: APena909 <apena76@student.rcc.edu>

---
+++ Homework/Assignment_2/Savitch_9thEd_Chap2_PracProg4/main.cpp
@@ -0,0 +1,36 @@
+/* 
+ * Author: Antonio Peña
+ * Purpose: Create a Mad Lib letter.
+ * April 4th, 2016
+ */
+
+//System Libraries
+#include <iostream>
+using namespace std;
+
+//User Libraries
+
+//Global Constants
+
+//Function Prototypes
+
+//Execution Begins Here
+int main(int argc, char** argv) {
+    //Declare Variables
+    short insName;//Instructor's name
+    short name;//Your name
+    short food;
+    int temp;//Number between 100 and 120
+    short adj;//Adjective
+    short color;
+    short animal;
+    
+    //Intro Message
+    cout<<"This program will create a Mad Lib letter using your "
+            "user-generated inputs!"<<endl;
+    
+    
+    //Exit Stage Right           
+    return 0;
+}
+
