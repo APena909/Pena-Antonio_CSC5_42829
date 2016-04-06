133d139d5846d8ceeb7eed665aadc4bb09da0080
APena909 <apena76@student.rcc.edu>
2016-04-06 10:31:46 -0700

    Signed-off-by: APena909 <apena76@student.rcc.edu>

--- Homework/Assignment_2/Savitch_9thEd_Chap2_PracProg4/main.cpp
+++ Homework/Assignment_2/Savitch_9thEd_Chap2_PracProg4/main.cpp
@@ -6,6 +6,7 @@
 
 //System Libraries
 #include <iostream>
+#include <string>
 using namespace std;
 
 //User Libraries
@@ -17,17 +18,59 @@ using namespace std;
 //Execution Begins Here
 int main(int argc, char** argv) {
     //Declare Variables
-    short insName;//Instructor's name
-    short name;//Your name
-    short food;
+    string insName;//Instructor's name
+    string youName;//Your name
+    string food;
     int temp;//Number between 100 and 120
-    short adj;//Adjective
-    short color;
-    short animal;
+    string adj;//Adjective
+    string color;
+    string animal;
     
     //Intro Message
     cout<<"This program will create a Mad Lib letter using your "
             "user-generated inputs!"<<endl;
+    
+    //Request Inputs
+    cout<<"Let's start with a few questions: "<<endl;
+    
+    cout<<"What is the first or last name of your instructor?"<<endl;
+    cin>>insName;
+    
+    cout<<"What is your name?"<<endl;
+    cin>>youName;
+    
+    cout<<"What is your favorite food?"<<endl;
+    cin>>food;
+    
+    cout<<"Enter a number between 100 and 120: "<<endl;
+    cin>>temp;
+    
+    cout<<"Type in an adjective: "<<endl;
+    cin>>adj;
+    
+    cout<<"What is your favorite color?"<<endl;
+    cin>>color;
+    
+    cout<<"Lastly, what is your favorite animal?"<<endl;
+    cin>>animal;
+    cout<<endl;
+    
+    //Output the Mad Lib letter
+    cout<<"Dear Instructor "<<insName<<","<<endl;
+    cout<<endl;
+    cout<<"I am sorry that I am unable to turn in my homework "
+            "at this time."<<endl;
+    cout<<"First, I ate a rotten "<<food<<", which made me turn "<<color<<" and"
+            " extremely ill."<<endl;
+    cout<<"I came down with a fever of "<<temp<<"."<<endl;
+    cout<<"Next, my "<<adj<<" pet "<<animal<<" must have smelled the remains"
+            " of the "<<food<<" on my "<<endl;
+    cout<<"homework because he ate it."<<endl;
+    cout<<"I am currently rewriting my homework and hope you will "
+            "accept it late."<<endl;
+    cout<<endl;
+    cout<<"Sincerely,"<<endl;
+    cout<<youName<<endl;
     
     
     //Exit Stage Right           
