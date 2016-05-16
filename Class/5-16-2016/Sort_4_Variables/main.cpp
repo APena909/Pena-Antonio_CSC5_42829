6e4920cd7a9ff18fb1201cea21eb60d5ce7919ac
APena909 <apena76@student.rcc.edu>
2016-05-16 09:47:02 -0700

    Signed-off-by: APena909 <apena76@student.rcc.edu>

---
+++ Class/5-16-2016/Sort_4_Variables/main.cpp
@@ -0,0 +1,17 @@
+/* 
+ * Author: Antonio Peña
+ * Purpose: Sort 4 Variables in correct order.
+ * Created on May 16, 2016, 8:57 AM
+ */
+
+#include <iostream>
+using namespace std;
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+
+    return 0;
+}
+
